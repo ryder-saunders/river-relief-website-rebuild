5b8834e238775ccc2d09cfcd3218f9bd06eb9ecd
ryder-saunders <ryder@riverrelief.com>
2026-09-18 14:03:19 -0700

    Update next.config.ts
    Redirects for Review Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,25 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/google",
+        destination: "https://g.page/r/Cdlk4n-kpZQVEAI/review",
+        permanent: false,
+      },
+      {
+        source: "/trustpilot",
+        destination: "https://www.trustpilot.com/evaluate/riverrelief.com",
+        permanent: false,
+      },
+      {
+        source: "/BBB",
+        destination: "https://www.bbb.org/santa-barbara/review/92097314/",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
